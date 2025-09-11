34c20060a0525b04238c575a42564866a8f6bb85
lutianyu <luty21@mails.tsinghua.edu.cn>
2025-09-11 08:16:21 +0000

    HK: 8 GPU script

--- scripts/HK/train.sh
+++ scripts/HK/train.sh
@@ -1,2 +1,2 @@
 export WANDB_API_KEY=bc2e2b14aacbadfd88a86ceab37243b8944b0eaf
-torchrun --nproc_per_node=1 trainer_new.py --config-name='folding'
+torchrun --nproc_per_node=8 trainer_new.py --config-name='folding'
