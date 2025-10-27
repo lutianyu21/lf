619ed4aba8bf063e371718b9eef610c2657c539c
lutianyu <luty.thu.coai@gmail.com>
2025-10-27 12:09:41 +0000

    HK: pickle part00

--- t.py
+++ t.py
@@ -5,30 +5,24 @@ import ray
 
 ray.init(log_to_driver=True)
 
-# main_pickle(
-#     src_dir="/GenSIvePFS/users/lutianyu/data/AFDB/part-02",
-#     dst_dir="/GenSIvePFS/users/lutianyu/data/AFDB/pickle/part-02",
-#     max_concurrent=1000,
-# )
 
-
-# step1_pickle(
-#     src_dir="/GenSIvePFS/users/lutianyu/lf/data/raw/cameo2022",
-#     dst_dir="/GenSIvePFS/users/lutianyu/lf/data/pickle/cameo2022",
-#     dataset_name="rcsb",
-#     clear=False, # for afdb only
-#     max_concurrent=2000,
-# )
-
-step2_parquet(
-    src_dir="/GenSIvePFS/users/lutianyu/lf/data/pickle/cameo2022",
-    dst_dir="/GenSIvePFS/users/lutianyu/lf/data/parquet/cameo2022",
-    tokenizer_name='dist',
-    num_cpu_workers=10,
-    num_gpu_workers=2,
-    batch_size=1000,
-    part_size=10000,
+step1_pickle(
+    src_dir="/GenSIvePFS/users/lutianyu/data/AFDB/part_00",
+    dst_dir="/GenSIvePFS/users/lutianyu/data/AFDB/pickle/part-00",
+    dataset_name="afdb",
+    clear=True, # for afdb only
+    max_concurrent=3000,
 )
+
+# step2_parquet(
+#     src_dir="/GenSIvePFS/users/lutianyu/lf/data/pickle/cameo2022",
+#     dst_dir="/GenSIvePFS/users/lutianyu/lf/data/parquet/cameo2022",
+#     tokenizer_name='dist',
+#     num_cpu_workers=10,
+#     num_gpu_workers=2,
+#     batch_size=1000,
+#     part_size=10000,
+# )
 
 
 # HK version
