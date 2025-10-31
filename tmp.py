ce2a3cec43ee65aabb4ada22b4e2a0bc4ac1f055
lutianyu <luty.thu.coai@gmail.com>
2025-10-31 03:29:24 +0000

    HK: rm

--- tmp.py
+++ tmp.py
@@ -42,29 +42,51 @@ logger.addHandler(handler)
 logger.setLevel(logging.INFO)
 logger.propagate = False
 
-src = "/GenSIvePFS/users/lutianyu/data/AFDB/pickle/part-00"
-dst1 = "/GenSIvePFS/users/lutianyu/data/AFDB/pickle/part_00"
-dst2 = "/GenSIvePFS/users/lutianyu/data/AFDB/pickle/part_01"
+# src = "/GenSIvePFS/users/lutianyu/data/AFDB/pickle/part-00"
+# dst1 = "/GenSIvePFS/users/lutianyu/data/AFDB/pickle/part_00"
+# dst2 = "/GenSIvePFS/users/lutianyu/data/AFDB/pickle/part_01"
 
-os.makedirs(dst1, exist_ok=True)
-os.makedirs(dst2, exist_ok=True)
+# os.makedirs(dst1, exist_ok=True)
+# os.makedirs(dst2, exist_ok=True)
 
-def iter_files(directory):
+# def iter_files(directory):
+#     with os.scandir(directory) as it:
+#         for entry in it:
+#             if entry.is_file():
+#                 yield entry.name
+
+# def move_file(i, name):
+#     dst = dst1 if i % 2 == 0 else dst2
+#     src_path = os.path.join(src, name)
+#     dst_path = os.path.join(dst, name)
+#     if not os.path.exists(dst_path):
+#         shutil.move(src_path, dst_path)
+#         logger.info(f"Moved file {src_path} to {dst_path}")
+#     else:
+#         logger.warning(f"File {dst_path} already exists. Skipping move.")
+
+# with ThreadPoolExecutor(max_workers=64) as pool:
+#     for i, name in enumerate(iter_files(src)):
+#         pool.submit(move_file, i, name)
+        
+
+
+def _delete_file(path):
+    try:
+        os.remove(path)
+        logger.warning(f"Deleted file: {path}")
+    except Exception as e:
+        logger.error(f"Failed to delete {path}: {e}")
+
+def _iter_files(directory):
     with os.scandir(directory) as it:
         for entry in it:
             if entry.is_file():
-                yield entry.name
+                yield entry.path
 
-def move_file(i, name):
-    dst = dst1 if i % 2 == 0 else dst2
-    src_path = os.path.join(src, name)
-    dst_path = os.path.join(dst, name)
-    if not os.path.exists(dst_path):
-        shutil.move(src_path, dst_path)
-        logger.info(f"Moved file {src_path} to {dst_path}")
-    else:
-        logger.warning(f"File {dst_path} already exists. Skipping move.")
-
-with ThreadPoolExecutor(max_workers=64) as pool:
-    for i, name in enumerate(iter_files(src)):
-        pool.submit(move_file, i, name)
+def delete_file(src_dir):
+    with ThreadPoolExecutor(max_workers=32) as pool:
+        for path in _iter_files(src_dir):
+            pool.submit(_delete_file, path)
+            
+delete_file("/GenSIvePFS/users/lutianyu/data/AFDB/pickle/part_00")
