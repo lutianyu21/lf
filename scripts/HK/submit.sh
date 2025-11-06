3d947827452db53b3ad07e0eef719582d80cd00e
lutianyu <luty.thu.coai@gmail.com>
2025-11-06 15:05:36 +0000

    HK: dist1 + qwen3-lm

--- scripts/HK/submit.sh
+++ scripts/HK/submit.sh
@@ -12,7 +12,7 @@
 #SBATCH --cpus-per-task=224             # CPUs/task
 #SBATCH --mem=2000                      # memory(MB)/node
 #SBATCH --export=ALL
-#SBATCH -w cp2-dgx-[010,016,017,031]
+#SBATCH -w cp2-dgx-[010,007,017,031]
 
 CONTAINER_PATH=/home/projects/protein/lutianyu/images/modern.sqsh
 CONTAINER_NAME=modern
