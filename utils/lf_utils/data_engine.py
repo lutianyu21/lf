6534592b254081fa46f75dae24a099f632d7d007
lutianyu <luty.thu.coai@gmail.com>
2025-11-30 12:35:30 +0000

    HK: scan

--- utils/lf_utils/data_engine.py
+++ utils/lf_utils/data_engine.py
@@ -1,11 +1,13 @@
-from pyexpat import features
 import ray
+import re
+import os
 from pathlib import Path
 import tarfile
 import shutil
 import colorlog
 import logging
-from typing import Iterator, Tuple, Any
+import pandas as pd
+from typing import Iterator, Optional, Tuple, Any
 
 
 __all__ = ['DataEngine']
@@ -32,27 +34,21 @@ logger.propagate = False
 @ray.remote
 def extract2target(iter: Any, output_dir: Path):
     output_dir.mkdir(parents=True, exist_ok=True)
+    tar_path, member_name = iter
     try:
-        if isinstance(iter, tuple):
-            tar_path, member_name = iter
-            with tarfile.open(tar_path, "r") as tf:
-                member = tf.getmember(member_name)
-                f = tf.extractfile(member)
-                if f is None:
-                    return ("failed", str(iter), "Failed to extract member")
-                
-                target_path = output_dir / Path(member.name).name
-                if target_path.exists():
-                    return ("skipped", str(target_path))
-                
-                with open(target_path, "wb") as out_f:
-                    shutil.copyfileobj(f, out_f)
-                return ("success", str(target_path))
-        else:
-            raise NotImplementedError()
-    
+        with tarfile.open(tar_path, "r") as tf:
+            member = tf.getmember(member_name)
+            f = tf.extractfile(member)
+            if f is None:
+                return ("failed", member_name, "Extracted file is None")
+            target_path = output_dir / Path(member.name).name
+            if target_path.exists():
+                return ("skipped", member_name)
+            with open(target_path, "wb") as out_f:
+                shutil.copyfileobj(f, out_f)
+            return ("success", member_name)
     except Exception as e:
-        return ("failed", str(iter), str(e))
+        return ("failed", member_name, str(e))
 
 
 # TODO: organization by sql ?
@@ -61,13 +57,20 @@ class DataEngine:
     ### Scanning Functions ###
     # scan AFDB / SwissProt / RCSB / CASP / CAMEO dataset and yield paths
     @classmethod
-    def _scan_hk_afdb(cls) -> Iterator[Tuple[Path, str]]:
+    def _scan_afdb(cls, tax_id: Optional[int] = None) -> Iterator[None | Tuple[Path, str]]:
         # e.g. for proteome-tax_id-1974607-0_v4.tar, we have
         # - AF-A0A2H0UIM4-F1-model_v4.cif.gz
         # - AF-A0A2H0UIM4-F1-confidence_v4.json.gz
         # - AF-A0A2H0UIM4-F1-predicted_aligned_error_v4.json.g[z
         # scanning will return 
         # - tmp_root/AF-A0A2H0UIM4-F1-model_v4.cif.gz
+        
+        # if tax_id is provided, filter by it proteome-tax_id-{tax_id}-{shard_id}_v4.tar
+        if tax_id is not None:
+            pattern = re.compile(fr"^proteome-tax_id-{tax_id}-\d+_v4\.tar$")
+        else:
+            pattern = re.compile(r"^proteome-tax_id-\d+-\d+_v4\.tar$")
+            
         for split_dir in [
             Path("/GenSIvePFS/users/lutianyu/data/AFDB/part_00"),
             Path("/GenSIvePFS/users/lutianyu/data/AFDB/part_01"),
@@ -77,24 +80,25 @@ class DataEngine:
             Path("/GenSIvePFS/users/lutianyu/data/AFDB/part_05_dest"),
             Path("/GenSIvePFS/users/lutianyu/data/AFDB/part_06"),
         ]:
-            for tar_path in split_dir.glob("proteome-tax_id-*_v4.tar"):
+            for tar_path in os.scandir(split_dir):
+                if not pattern.match(tar_path.name): continue
                 try:
                     with tarfile.open(tar_path, "r") as tf:
                         for member in tf:
                             if member.name.endswith("-F1-model_v4.cif.gz"):
                                 # Workers will take care of tmp files
-                                yield (tar_path, member.name)
+                                yield (Path(tar_path.path), member.name)
                 except Exception as e:
-                    logger.error(f"Failed to scan {tar_path}: {e}")
+                    logger.error(f"Failed to scan {tar_path.path}: {e}")
                     continue
-                    
     
     ### API ###
-    def _query_hk_afdb(
+    def _query_afdb(
         self,
-        query_path: Path,
         output_dir: Path,
         max_concurrent: int,
+        query_path: Path,           # a .txt file containing list of accession ids
+        bq_path: Optional[Path],    # a .parquet file containing `taxId` `uniprotAccession`
     ):
         if not query_path.name.endswith('.txt'): raise NotImplementedError()
         output_dir.mkdir(parents=True, exist_ok=True)
@@ -107,44 +111,53 @@ class DataEngine:
                     if not item.startswith('AF'):
                         continue
                     else:
-                        # FIX here: uniref-style AF_AFA0A075B5T2F1_1; HK-style AF-A0A075B5T2-F1-model_v4
+                        # FIX here: rcsb-style AF_AFA0A075B5T2F1_1 >> uniref-style AF-A0A075B5T2-F1-model_v4
                         item_fixed = f"AF-{item.split('_')[1][2:-2]}-F1-model_v4"
                         query_set.add(item_fixed)
-        
-        # submit cpu jobs
-        total_count = 0
-        failures,futures = [], []
-        for i, it in enumerate(self._scan_hk_afdb()):
-            if isinstance(it, tuple):
-                tar_path, member_name = it
-                p_name = Path(member_name).name
-                p_name = Path(p_name).stem      # remove .gz
-                p_name = Path(p_name).stem      # remove .cif
-            else:
-                raise NotImplementedError()
+                        
+        if bq_path is None:
+            logger.warning("No tax_path provided, querying all AFDB entries ...")
+            # TODO a effective way to load all taxId and uniprotAccession from AFDB
+            raise NotImplementedError()
+        else:
+            bq_df = pd.read_parquet(bq_path)
+            bq_df['uniprotAccession'] = bq_df['uniprotAccession'].apply(lambda x: f"AF-{x}-F1-model_v4")
             
-            if p_name not in query_set:
-                logger.warning(f"[uid={i}] Skipping {p_name} ...")
-                continue
-            else:
-                logger.info(f"[uid={i}] Collecting {p_name} ...")
+        grouped_queries = bq_df.groupby('taxId')['uniprotAccession'].apply(set).to_dict()
+        hit_count, futures, failures = 0, [], []
+        for tax_id, required_accessions in grouped_queries.items():
             
-            # if match, submit a copy/parsing task
-            futures.append(extract2target.remote(it, output_dir))
-            if len(futures) >= max_concurrent:
-                done, futures = ray.wait(futures, num_returns=1)
-                done_results = ray.get(done)
-                for res in done_results:
-                    status = res[0]
-                    if status == "success":
-                        total_count += 1
-                        logger.info(f"[{total_count}/{len(futures)}] Processed: {res[1]}")
-                    elif status == "skipped":
-                        logger.warning(f"[{total_count}/{len(futures)}] Skipped (exists): {res[1]}")
-                    elif status == "failed":
-                        logger.error(f"[{total_count}/{len(futures)}] Failed: {res[1]} Error: {res[2]}")
-                        failures.append(res[1])
+            logger.info(f"Scanning AFDB for Tax ID: {tax_id} (Targeting {len(required_accessions)} proteins)")
+            # {tax-id : set(uniprotAccessions)}
+            for it in self._scan_afdb(tax_id=tax_id):
+                if isinstance(it, tuple):
+                    tar_path, member_name = it
+                    # remove.cif.gz，to get accession ID
+                    p_name = Path(Path(member_name).name).stem
+                    p_name = Path(p_name).stem 
+                else:
+                    raise NotImplementedError()
 
+                # submit tasks only if in required_accessions
+                if p_name in required_accessions:
+                    futures.append(extract2target.remote(it, output_dir))
+                
+                # reduce memory pressure
+                if len(futures) >= max_concurrent:
+                    done, futures = ray.wait(futures, num_returns=1)
+                    done_results = ray.get(done)
+                    for res in done_results:
+                        status = res[0]
+                        if status == "success":
+                            hit_count += 1
+                            logger.info(f"[{hit_count}/{len(query_set)}] Processed: {res[1]}")
+                        elif status == "skipped":
+                            hit_count += 1
+                            logger.warning(f"[{hit_count}/{len(query_set)}] Skipped: {res[1]}")
+                        elif status == "failed":
+                            failures.append(res[1])
+                            logger.error(f"[{hit_count}/{len(query_set)}] Failed: {res[1]} reason: {res[2]}")
+                            
         # collecting remaining futures
         while futures:
             done, futures = ray.wait(futures)
@@ -152,14 +165,16 @@ class DataEngine:
             for res in done_results:
                 status = res[0]
                 if status == "success":
-                    total_count += 1
-                    logger.info(f"[{total_count}] Processed: {res[1]}")
+                    hit_count += 1
+                    logger.info(f"[{hit_count}/{len(query_set)}] Processed: {res[1]}")
                 elif status == "skipped":
-                    logger.warning(f"[{total_count}] Skipped (exists): {res[1]}")
+                    hit_count += 1
+                    logger.warning(f"[{hit_count}/{len(query_set)}] Skipped (exists): {res[1]}")
                 elif status == "failed":
-                    logger.error(f"[{total_count}] Failed: {res[1]} Error: {res[2]}")
                     failures.append(res[1])
+                    logger.error(f"[{hit_count}/{len(query_set)}] Failed: {res[1]} reason: {res[2]}")
                     
+        # HINT: in current design, we can only ensure that all [existed/queried] items are processed
         failures_file = output_dir / "failures.txt"
         if failures_file.exists(): failures_file.unlink()
         if failures:
@@ -167,4 +182,4 @@ class DataEngine:
                 for item in failures:
                     f.write(f"{item}\n")
             logger.info(f"Total failed items: {len(failures)}. Saved to {failures_file}")
-        logger.info(f"All tasks completed. Total submitted: {total_count}")
+        logger.info(f"All tasks completed. Total queried: {hit_count}/{len(query_set)}")
--- x.py
+++ x.py
@@ -4,8 +4,9 @@ ray.init(ignore_reinit_error=True)
 from utils.lf_utils.data_engine import DataEngine
 from pathlib import Path
 
-DataEngine()._query_hk_afdb(
+DataEngine()._query_afdb(
     query_path=Path("/GenSIvePFS/users/lutianyu/lf/clusters-by-entity-40.txt"),
+    bq_path=Path("/GenSIvePFS/users/lutianyu/lf/bq.parquet"),
     output_dir=Path("/GenSIvePFS/users/lutianyu/lf/afdb"),
     max_concurrent=8000,
 )
