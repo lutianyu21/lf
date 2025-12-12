b5bb2e8bf216871545543b8a247d0d124dbcae31
lutianyu <luty.thu.coai@gmail.com>
2025-12-12 10:40:23 +0000

    DEBUG: tid

--- utils/lf_utils/trainer.py
+++ utils/lf_utils/trainer.py
@@ -591,7 +591,8 @@ AR v.s. Nature: TM-score =  {tm_ar:.4f}, RMSD_L = {rmsd_l_ar:.4f}, RMSD_G = {rms
     def compute_metrics(cls, eval_pred: EvalPrediction):
         preds: Dict[str, np.ndarray] = eval_pred.predictions # type: ignore
         df = pd.DataFrame({k: v for k, v in preds.items()})
-        logger.error(df)
+        # print df['tid'].unique()
+        logger.error(df['tid'].unique())
         df['tid'] = df['tid'].astype(int)
         metrics = {}
         # group dataframe by tid
